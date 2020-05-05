28ff412f00e2cf137d9dafb57a48d45a862857cb
olivier.lediouris <olivier.lediouris@oracle.com>
2020-05-05 12:48:42 -0700

    Struggling with module-info

--- opencv/swing.camera.sh
+++ opencv/swing.camera.sh
@@ -2,7 +2,7 @@
 CP=./build/libs/opencv-1.0-all.jar
 # CP="$CP:/usr/local/Cellar/opencv/4.1.0_2/share/java/opencv4/opencv-410.jar" # Mac
 CP="$CP:/usr/local/share/java/opencv4/opencv-430.jar" # RPi
-JAVA_LIB_PATH="/usr/local/Cellar/opencv/4.1.0_2/share/java/opencv4" # For Mac
+# JAVA_LIB_PATH="/usr/local/Cellar/opencv/4.1.0_2/share/java/opencv4" # For Mac
 JAVA_LIB_PATH="/usr/local/share/java/opencv4" # RPi
 CLASS=oliv.opencv.OpenCVSwingCamera
 #
