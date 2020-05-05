aedaa45882461bb210af3850c89d80b3c439cef0
olivier.lediouris <olivier.lediouris@oracle.com>
2020-05-05 12:06:03 -0700

    Shooting in the dark

--- opencv/src/main/java/module-info.java
+++ opencv/src/main/java/module-info.java
@@ -3,5 +3,12 @@ module oliv.ai.opencv.main {
 	requires javafx.controls;
 	requires javafx.swing;
 
+	exports java.util.logging;
+	exports org.opencv.core;
+	exports org.opencv.imgcodecs;
+	exports org.opencv.imgproc;
+	exports org.opencv.videoio;
+
 	opens oliv.opencv;
+	opens it.polito.elite.teaching.cv.dummy;
 }
