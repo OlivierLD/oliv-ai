136e4052758164f02acfc1212f4bbc93d684b721
olivier.lediouris <olivier.lediouris@oracle.com>
2020-05-05 11:02:27 -0700

    Added module-info files

---
+++ oliv-opencv/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module oliv.ai.oliv.opencv.main {
+	requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.swing;
+
+	opens opencv;
+}
---
+++ opencv/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module oliv.ai.opencv.main {
+	requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.swing;
+
+	opens oliv.opencv;
+}
