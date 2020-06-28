bf6f5d3724ab0bac1b853c011e89d84df301b078
olivier.lediouris <olivier.lediouris@oracle.com>
2020-06-28 09:35:25 -0700

    Scripts

--- opencv/swing.camera.sh
+++ opencv/swing.camera.sh
@@ -6,7 +6,8 @@ JAVA_LIB_PATH="/usr/local/Cellar/opencv/4.3.0_5/share/java/opencv4" # For Mac
 # JAVA_LIB_PATH="/usr/local/share/java/opencv4" # RPi
 CLASS=oliv.opencv.OpenCVSwingCamera
 #
-# For MacOS Catalina
-JAVA_OPTS="-DOPENCV_AVFOUNDATION_SKIP_AUTH=1"
+# For MacOS Catalina, does not work :(
+# JAVA_OPTS="-DOPENCV_AVFOUNDATION_SKIP_AUTH=1"
+JAVA_OPTS="-Dinput.source=file:///Users/olivierlediouris/repos/oliv-ai/opencv/images/birds/java.sparrows.jpg"
 #
 java -cp ${CP} -Djava.library.path=${JAVA_LIB_PATH} ${JAVA_OPTS} ${CLASS}
