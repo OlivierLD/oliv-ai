14698728af7e28b74e3339499896fb10150b384e
olivier.lediouris <olivier.lediouris@oracle.com>
2020-08-13 07:24:23 -0700

    Added Java kernel

--- opencv/swing.camera.sh
+++ opencv/swing.camera.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 CP=./build/libs/opencv-1.0-all.jar
-CP="$CP:/usr/local/Cellar/opencv/4.3.0_5/share/java/opencv4/opencv-430.jar" # Mac
-# CP="$CP:/usr/local/share/java/opencv4/opencv-430.jar" # RPi
-JAVA_LIB_PATH="/usr/local/Cellar/opencv/4.3.0_5/share/java/opencv4" # For Mac
-# JAVA_LIB_PATH="/usr/local/share/java/opencv4" # RPi
+JAVA_LIB_PATH=
+DARWIN=$(uname -a | grep Darwin)  # Mac ?
+#
+if [[ "$DARWIN" != "" ]]
+then
+  CP="$CP:/usr/local/Cellar/opencv/4.3.0_5/share/java/opencv4/opencv-430.jar" # Mac
+  JAVA_LIB_PATH="/usr/local/Cellar/opencv/4.3.0_5/share/java/opencv4" # For Mac
+else  # Assume Raspberry Pi
+  CP="$CP:/usr/local/share/java/opencv4/opencv-430.jar" # RPi
+  JAVA_LIB_PATH="/usr/local/share/java/opencv4" # RPi
+fi
+#CP="$CP:/usr/local/Cellar/opencv/4.3.0_5/share/java/opencv4/opencv-430.jar" # Mac
+## CP="$CP:/usr/local/share/java/opencv4/opencv-430.jar" # RPi
+#JAVA_LIB_PATH="/usr/local/Cellar/opencv/4.3.0_5/share/java/opencv4" # For Mac
+## JAVA_LIB_PATH="/usr/local/share/java/opencv4" # RPi
+#
 CLASS=oliv.opencv.OpenCVSwingCamera
 #
 # For MacOS Catalina, does not work :(
