828026194769e393a38ff1bc0f4e023228a3c7a4
olivier.lediouris <olivier.lediouris@oracle.com>
2020-09-13 13:05:33 -0700

    Face detection, remote debugging

--- opencv/face.detection.sh
+++ opencv/face.detection.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 CP=./build/classes/java/main
 CP="$CP:./build/libs/opencv-1.0-all.jar"
-CP="$CP:/usr/local/Cellar/opencv/4.1.0_2/share/java/opencv4/opencv-410.jar"
-JAVA_LIB_PATH="/usr/local/Cellar/opencv/4.1.0_2/share/java/opencv4" # For Mac
+#
+DARWIN=$(uname -a | grep Darwin)  # Mac ?
+#
+if [[ "$DARWIN" != "" ]]
+then
+  CP="$CP:/usr/local/Cellar/opencv/4.3.0_5/share/java/opencv4/opencv-430.jar" # Mac
+  JAVA_LIB_PATH="/usr/local/Cellar/opencv/4.3.0_5/share/java/opencv4"         # For Mac
+else  # Assume Raspberry Pi
+  CP="$CP:/usr/local/share/java/opencv4/opencv-430.jar" # RPi
+  JAVA_LIB_PATH="/usr/local/share/java/opencv4"         # RPi
+fi
+#
 CLASS=oliv.opencv.OpenCVContinuousFaceDetection
 #
 java -cp ${CP} -Djava.library.path=${JAVA_LIB_PATH} ${CLASS}
