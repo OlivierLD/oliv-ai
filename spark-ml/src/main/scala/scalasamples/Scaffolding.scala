a1e8d31590d36ad45d29cee0b528e30ca018801d
olivier.lediouris <olivier.lediouris@oracle.com>
2020-10-12 12:12:04 -0700

    Spark, python, scala, java

--- spark-ml/src/main/scala/scalasamples/Scaffolding.scala
+++ spark-ml/src/main/scala/scalasamples/Scaffolding.scala
@@ -6,20 +6,22 @@ import org.apache.spark.sql.Dataset
 
 
 object Scaffolding {
-  private val DATA_PATH: String = s"file://${System.getProperty("user.dir")}/duocar/raw/rides"
+  private val RIDES_PATH: String = s"file://${System.getProperty("user.dir")}/duocar/raw/rides"
+//  private val RIDERS_PATH: String = s"file://${System.getProperty("user.dir")}/duocar/raw/riders"
 
   def main(args:Array[String]): Unit = {
     val spark: SparkSession = SparkSession.builder
                                           .appName("SimpleApp")
                                           .config("spark.master", "local")
                                           .getOrCreate
-    val csv: Dataset[Row] = spark.read
-                                 .option("delimiter", ",")
-                                 .option("inferSchema", true)
-                                 .option("header", true)
-                                 .csv(DATA_PATH)
-    println(s"Count: ${csv.count}")
-    csv.printSchema()
+    val rides: Dataset[Row] = spark.read
+                                   .option("delimiter", ",")
+                                   .option("inferSchema", true)
+                                   .option("header", true)
+                                   .csv(RIDES_PATH)
+    println(s"Count: ${rides.count}")
+    rides.printSchema()
+
     spark.close()
   }
 }
