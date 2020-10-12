c10b702b045a0e7d9aad4ca11cd62f8421720873
olivier.lediouris <olivier.lediouris@oracle.com>
2020-10-12 10:08:08 -0700

    Spark, python, scala, java

--- spark-ml/src/main/java/javasamples/Scaffolding.java
+++ spark-ml/src/main/java/javasamples/Scaffolding.java
@@ -1,5 +1,6 @@
 package javasamples;
 
+import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
@@ -13,14 +14,28 @@ public class Scaffolding {
                 .appName("SimpleApp")
                 .config("spark.master", "local")
                 .getOrCreate();
-        Dataset<Row> csv = spark.read()
+        Dataset<Row> rides = spark.read()
                 .option("delimiter", ",")
                 .option("inferSchema", true)
                 .option("header", true)
                 .csv(DATA_PATH);
-        System.out.println(String.format("Count: %d", csv.count()));
-        csv.printSchema();
-
+        System.out.println(String.format("Count: %d", rides.count()));
+        System.out.println("Print inferred schema:");
+        rides.printSchema();
+        System.out.println("Show 5");
+        rides.show(5);
+        rides.show(5, false);
+        boolean vertical = true;
+        rides.show(5, 1, vertical);
+        System.out.println("Describe");
+        Dataset<Row> limit5 = rides.limit(5);
+        try {
+            limit5.describe("service", "distance", "duration", "cancelled")
+                  .show();
+        } catch (Exception ae) {
+            ae.printStackTrace();
+        }
+        System.out.println("Done!");
         spark.close();
     }
 }
