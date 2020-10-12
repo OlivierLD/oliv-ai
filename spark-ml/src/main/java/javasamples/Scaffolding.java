0662a0d88ab619bba23c5121435a39a034a6a3db
olivier.lediouris <olivier.lediouris@oracle.com>
2020-10-12 12:07:43 -0700

    Spark, python, scala, java

--- spark-ml/src/main/java/javasamples/Scaffolding.java
+++ spark-ml/src/main/java/javasamples/Scaffolding.java
@@ -1,13 +1,12 @@
 package javasamples;
 
-import org.apache.spark.sql.AnalysisException;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.*;
+import static org.apache.spark.sql.functions.col;
 
 public class Scaffolding {
 
-    private final static String DATA_PATH = String.format("file://%s/duocar/raw/rides", System.getProperty("user.dir"));
+    private final static String RIDES_PATH = String.format("file://%s/duocar/raw/rides", System.getProperty("user.dir"));
+    private final static String RIDERS_PATH = String.format("file://%s/duocar/raw/riders", System.getProperty("user.dir"));
 
     public static void main(String... args) {
         SparkSession spark = SparkSession.builder()
@@ -18,7 +17,7 @@ public class Scaffolding {
                 .option("delimiter", ",")
                 .option("inferSchema", true)
                 .option("header", true)
-                .csv(DATA_PATH);
+                .csv(RIDES_PATH);
         System.out.println(String.format("Count: %d", rides.count()));
         System.out.println("Print inferred schema:");
         rides.printSchema();
@@ -35,6 +34,60 @@ public class Scaffolding {
         } catch (Exception ae) {
             ae.printStackTrace();
         }
+
+        System.out.println("--- R I D E R S ---");
+        Dataset<Row> riders = spark.read()
+                .option("delimiter", ",")
+                .option("inferSchema", true)
+                .option("header", true)
+                .csv(RIDERS_PATH);
+        Dataset<Row> select = riders.select("birth_date", "student", "sex");
+        select.printSchema();
+        select.show(5);
+
+        // Adding a column
+        try {
+            Dataset<Row> rowDataset = riders.select("birth_date", "student", "sex")
+                    .withColumn("student_bool", select.col("student").equalTo(1));
+            rowDataset.show(5);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        // Adding a column, method B
+        try {
+            Dataset<Row> select2 = riders.select(col("student").equalTo(1).alias("student_bool"), col("birth_date"), col("sex"));
+            select2.printSchema();
+            select2.show(5);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        // Adding a column, method C
+        try {
+            riders.selectExpr("student", "student = 1 as student_boolean").show(5);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        // Adding a column, method D
+        try {
+            riders.createOrReplaceTempView("riders_view");
+            spark.sql("select student, student = 1 as student_boolean from riders_view").show(5);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        // Filtering
+        try {
+            Dataset<Row> select2 = riders.select(col("student").equalTo(1).alias("student_bool"),
+                    col("birth_date"), col("sex"))
+                    .filter(col("student_bool").equalTo(true))
+                    .where(col("sex").isNotNull()); // .where() does the same thing
+            select2.printSchema();
+            select2.show(5);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        // Etc... sample, sampleBy, groupBy, count, dropna, fillNa, drop, replace
+
         System.out.println("Done!");
         spark.close();
     }
