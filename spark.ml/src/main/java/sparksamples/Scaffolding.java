9bfeeebedde1200de33ff730b795ca8436a68768
olivier.lediouris <olivier.lediouris@oracle.com>
2020-10-12 09:20:21 -0700

    Spark, python, scala, java

---
+++ spark.ml/src/main/java/sparksamples/Scaffolding.java
@@ -0,0 +1,26 @@
+package sparksamples;
+
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+
+public class Scaffolding {
+
+    private final static String DATA_PATH = String.format("file://%s/duocar/raw/rides", System.getProperty("user.dir"));
+
+    public static void main(String... args) {
+        SparkSession spark = SparkSession.builder()
+                .appName("SimpleApp")
+                .config("spark.master", "local")
+                .getOrCreate();
+        Dataset<Row> csv = spark.read()
+                .option("delimiter", ",")
+                .option("inferSchema", true)
+                .option("header", true)
+                .csv(DATA_PATH);
+        System.out.println(String.format("Count: %d", csv.count()));
+        csv.printSchema();
+
+        spark.close();
+    }
+}
