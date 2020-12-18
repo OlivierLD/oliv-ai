6bea8be266a3e98852890f12e37c625104892f1a
olivier.lediouris <olivier.lediouris@gmail.com>
2020-12-18 12:02:08 -0800

    OCR, with MicroSoft FormRecognizer

--- ocr/src/main/java/oliv/MSFormRecognizer.java
+++ ocr/src/main/java/oliv/MSFormRecognizer.java
@@ -8,10 +8,7 @@ package oliv;
  */
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
+import org.apache.http.*;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -45,6 +42,9 @@ public class MSFormRecognizer {
 
     private static final String DEFAULT_MS_BASE_URL = "https://svformrecognizer.cognitiveservices.azure.com";
     private static final String DEFAULT_MS_OCR_KEY = "a5a78d4392fb4b159739349cc228e97f";
+
+    private static final String SUBSCRIPTION_KEY_HEADER = "Ocp-Apim-Subscription-Key";
+    private final static String OPERATION_LOCATION = "Operation-Location";
 
     private static class ProcessContent {
 
@@ -93,13 +93,11 @@ public class MSFormRecognizer {
             log.info(String.format("Will connect to %s", firstEndPoint));
 
             HttpPost ocrRequestOne = new HttpPost(firstEndPoint);
-            ocrRequestOne.setHeader("Ocp-Apim-Subscription-Key", msKey.trim());
+            ocrRequestOne.setHeader(SUBSCRIPTION_KEY_HEADER, msKey.trim());
 
             if (processContent.getContentType().equals(ProcessContent.ContentType.IMAGE_URL)) {
-                ocrRequestOne.setHeader("Content-Type", ContentType.APPLICATION_JSON.getMimeType());
+                ocrRequestOne.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType());
                 // The body
-                //            String imageURL =
-                // "https://objectstorage.us-phoenix-1.oraclecloud.com/p/DmZ4WQcONbq7iDcr5UMxezvMZ33HTexL8GpvR0L7RzHYessER0fgUxg8bxdLAM_M/n/devdigital/b/large-receipts/o/1000-receipt.jpg";
                 String imageURL = processContent.getLocation();
                 String payload = String.format("{ \"source\": \"%s\" }", imageURL);
                 log.info(String.format("Image URL: %s", payload));
@@ -108,22 +106,26 @@ public class MSFormRecognizer {
                 HttpEntity entity = ocrRequestOne.getEntity();
                 log.info(String.format("Entity: %s", entity.toString()));
             } else if (processContent.getContentType().equals(ProcessContent.ContentType.IMAGE_PATH)) {
-                ocrRequestOne.setHeader("Content-Type", ContentType.APPLICATION_OCTET_STREAM.getMimeType());
-
-                BufferedImage bImage = ImageIO.read(new File(processContent.getLocation()));
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                ImageIO.write(bImage, findExtension(processContent.getLocation()), baos);
-                byte[] bytes = baos.toByteArray();
-                ByteArrayEntity entity = new ByteArrayEntity(baos.toByteArray());
-                ocrRequestOne.setEntity(entity);
-                log.info(String.format("Ext %s, %d bytes.", findExtension(processContent.getLocation()), bytes.length));
-
+                ocrRequestOne.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_OCTET_STREAM.getMimeType());
+                // The body
+                try {
+                    BufferedImage bImage = ImageIO.read(new File(processContent.getLocation()));
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    ImageIO.write(bImage, findExtension(processContent.getLocation()), baos);
+                    byte[] bytes = baos.toByteArray();
+                    ByteArrayEntity entity = new ByteArrayEntity(baos.toByteArray());
+                    ocrRequestOne.setEntity(entity);
+                    log.info(String.format("Ext %s, %d bytes.", findExtension(processContent.getLocation()), bytes.length));
 //                MultipartEntityBuilder builder = MultipartEntityBuilder.create();
 //                builder.addBinaryBody(
 //                        "file", bytes, ContentType.APPLICATION_OCTET_STREAM, processContent.getLocation());
 //
 //                HttpEntity multipart = builder.build();
 //                ocrRequestOne.setEntity(multipart);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    System.exit(1);
+                }
             }
 
             CloseableHttpClient client = HttpClientBuilder.create().build();
@@ -139,15 +141,15 @@ public class MSFormRecognizer {
             String responseAsString = EntityUtils.toString(response.getEntity());
             System.out.println(responseAsString);
             // Check the other URL in the response's headers
-            Header[] headers = response.getHeaders("Operation-Location");
+            Header[] headers = response.getHeaders(OPERATION_LOCATION);
             String nextUrl;
             if (headers != null && headers.length == 1) {
                 nextUrl = headers[0].getValue();
                 if (nextUrl != null) {
                     log.info(String.format("Send URL: %s", nextUrl));
                     HttpGet ocrRequestTwo = new HttpGet(nextUrl);
-                    ocrRequestTwo.setHeader("Accept", "*/*");
-                    ocrRequestTwo.setHeader("Ocp-Apim-Subscription-Key", msKey.trim());
+                    ocrRequestTwo.setHeader(HttpHeaders.ACCEPT, "*/*");
+                    ocrRequestTwo.setHeader(SUBSCRIPTION_KEY_HEADER, msKey.trim());
                     if (withProxy) {
                         ocrRequestTwo.setConfig(config); // For the proxy
                     }
@@ -206,12 +208,21 @@ public class MSFormRecognizer {
     private final static String imageURL = "https://upload.wikimedia.org/wikipedia/commons/0/0b/ReceiptSwiss.jpg";
     private final static String imagePath = "/Users/olivierlediouris/repos/oliv-ai/OpenCV-doc-processing/FormProcessingSampleData/gas.receipt.jpg";
 
+    private final static String PROXY_PRM_PREFIX = "--use-proxy:";
+
     public static void main(String... args) {
         ProcessContent[] images = new ProcessContent[]{
                 new ProcessContent(ProcessContent.ContentType.IMAGE_URL, imageURL),
                 new ProcessContent(ProcessContent.ContentType.IMAGE_PATH, imagePath)
         };
         boolean useProxy = false;
+        if (args.length > 0) {
+            for (String arg : args) {
+                if (arg.startsWith(PROXY_PRM_PREFIX)) {
+                    useProxy = arg.substring(PROXY_PRM_PREFIX.length()).equals("true");
+                }
+            }
+        }
 
         for (ProcessContent processContent : images) {
             String parsedContent = reachOutToMsOcrService(processContent, useProxy);
