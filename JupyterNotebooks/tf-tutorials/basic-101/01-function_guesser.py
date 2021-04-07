0363944d12de551494febdaac50f89c6e78ff5bb
olivier.lediouris <olivier.lediouris@gmail.com>
2021-04-07 14:11:30 -0700

    Small tweaks

--- JupyterNotebooks/tf-tutorials/basic-101/01-function_guesser.py
+++ JupyterNotebooks/tf-tutorials/basic-101/01-function_guesser.py
@@ -21,7 +21,9 @@ plot_model(model,
            show_shapes=True,
            show_layer_names=True)
 
+#
 # Actual function is y = (2x - 1)
+#
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)   # in
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)  # out, labels.
 
@@ -36,20 +38,24 @@ show_details = True
 if show_details:  # Display model details
     json_string = model.to_json()
     parsed_json = json.loads(json_string)
-    print("Model, json format:\n{}".format(json.dumps(parsed_json, indent=4)))
+    # print("Model, json format:\n{}".format(json.dumps(parsed_json, indent=4)))
+    print(f"Model, json format:\n{json.dumps(parsed_json, indent=4)}")
     for layer in model.layers:
         try:
             weights = layer.get_weights()[0]
             biases = layer.get_weights()[1]
-            print("Weights: {}\nBiases: {}".format(weights, biases))
+            # print("Weights: {}\nBiases: {}".format(weights, biases))
+            print(f"Weights: {weights}\nBiases: {biases}")
         except Exception:
             print("Oops")
 
 model.summary()
 
-prediction = model.predict([10.0])
+x = 10.0
+prediction = model.predict([x])
 # print("For value 10, predict is {}".format(prediction[0][0]))
 print(f"For value 10, predict is {prediction[0][0]}")
-expected = (2 * 10.0) - 1
+expected = (2 * x) - 1
 error = (expected - prediction[0][0]) / expected
 print("Expected {}, error is {:.3f}% ({}%)".format(expected, error * 100, error * 100))
+print(f"... Calculated: { weights[0][0] }.x + { biases[0] } = { (weights[0][0] * x) + biases[0] }")
