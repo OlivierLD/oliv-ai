914831655934e042181a93a676ea295bfb9f0279
olivier.lediouris <olivier.lediouris@gmail.com>
2021-04-30 11:15:39 -0700

    Stuff...

--- opencv/src/main/java/docai/opencv/OpenCVSwingReadURL.java
+++ opencv/src/main/java/docai/opencv/OpenCVSwingReadURL.java
@@ -5,7 +5,7 @@ import docai.opencv.swing.SwingFrame;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 
-import java.awt.Dimension;
+import java.awt.*;
 
 /**
  * Oliv did it.
@@ -18,6 +18,8 @@ import java.awt.Dimension;
 public class OpenCVSwingReadURL {
 
 	private final static String IMAGE_URL = "http://donpedro.lediouris.net/journal/trip/PB180629.JPG";
+	private final static int DEFAULT_FRAME_WIDTH =  1_024;
+	private final static int DEFAULT_FRAME_HEIGHT =   760;
 
 //	private final static long WAIT = 5_000L;
 
@@ -26,19 +28,30 @@ public class OpenCVSwingReadURL {
 	public static void process(String imageUrl) {
 
 		swingFrame = new SwingFrame();
+		swingFrame.setTitle("OpenCV read image fro URL");
+
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		System.out.println(String.format("Screen Dimensions: w %d h %d", screenSize.width, screenSize.height));
+
+		Dimension frameSize = new Dimension(Math.min(DEFAULT_FRAME_WIDTH, screenSize.width),
+											Math.min(DEFAULT_FRAME_HEIGHT, screenSize.height));
+		swingFrame.setSize(frameSize);
+		int x = (int) ((screenSize.getWidth() - swingFrame.getWidth()) / 2);
+		int y = (int) ((screenSize.getHeight() - swingFrame.getHeight()) / 2);
+		swingFrame.setLocation(x, y);
 		swingFrame.setVisible(true);
 
 		int padding = 30;
 
 		try {
 			Mat image = Utils.readMatFromURL(imageUrl);
-			swingFrame.setSize(new Dimension(image.width(), image.height() + padding));
-			swingFrame.setPreferredSize(new Dimension(image.width(), image.height() + padding));
+//			swingFrame.setSize(new Dimension(image.width(), image.height() + padding));
+//			swingFrame.setPreferredSize(new Dimension(image.width(), image.height() + padding));
 
 			System.out.println(String.format("Original image: w %d, h %d, channels %d", image.width(), image.height(), image.channels()));
 
 			System.out.println("-> Original");
-			swingFrame.plot(Utils.mat2AWTImage(image), "Original");
+			swingFrame.plot(Utils.mat2AWTImage(image), "Original - from its URL on the Web");
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
