cf39ebcc10421e53d07cfc4d6e20a21a0714cb61
olivier.lediouris <olivier.lediouris@gmail.com>
2021-05-14 14:52:56 -0700

    With python code

---
+++ nltk/nltk.101.py
@@ -0,0 +1,11 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_string = """
+Muad'Dib learned rapidly because his first training was in how to learn.
+And the first lesson of all was the basic trust that he could learn.
+It's shocking to find how many people do not believe they can learn,
+and how many more believe learning to be difficult."""
+
+sent_tokenize(example_string)
+
+word_tokenize(example_string)
